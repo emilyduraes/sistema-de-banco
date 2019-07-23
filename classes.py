89900c2c802ca97bad860c26d62584d3daaf1d6e
emilyduraes <emilyduraes@yahoo.com.br>
2019-07-23 17:05:15 -0300

    adicionar rascunho do projeto em comentário

--- classes.py
+++ classes.py
@@ -38,3 +38,29 @@ class Banco():
 #        métodos: saque, depósito, consulta de saldo, criar conta (novo usuário), validar usuário
 # interface:
 #        atributos:  bancos
+
+# ---------------------------------------------------------
+
+# 1. O sistema permite que o cliente se cadastre:
+#   "Digite seu nome:" input
+#   "Digite seu cpf:" input
+
+# 2. O sistema cria uma conta automaticamente para o cliente
+#   "Olá João, sua conta é 1234-1" *fun aleatório *
+
+# 3. Após a criação da conta, o sistema exibe um menu: input + (fun 1 2 3)
+#   "1- Consultar saldo"
+#   "2- Depositar"
+#   "3- Sacar"
+# *if *
+# Caso o cliente escolha a opção 1:
+#   "Seu saldo é R$ 0" *print *
+# Caso o cliente escolha a opção 2:
+#   "Digite o valor: " input
+#   "Depósito efetuado. Seu novo saldo é R$ 500" *print saldo+depósito *
+# Caso o cliente escolha a opção 3:
+#   "Digite o valor: " *input *
+#   "Saque efetuado. Seu novo saldo é R$ 100" print saldo - saque
+# Caso o cliente escolha a opção 3 e o saldo seja insuficiente: *else if saldo insuficiente *
+#   "Digite o valor: " *input *
+#   "Saldo insuficiente" *print *
